db30fa8ebf2f1e3b1a5130620c0a43f719c59e0c
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-03-23 00:58:22 +0200

    add all

---
+++ src/js/2-form.js
@@ -0,0 +1,46 @@
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+const email = form.querySelector("input[name='email']");
+const textArea = form.querySelector("textarea");
+
+const formData = { email: "", message: "" };
+
+form.addEventListener("submit", handleSubmit);
+textArea.addEventListener("input", handleInput);
+email.addEventListener("input", handleInput);
+
+populateForm();
+
+function handleInput(event) {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    if(!formData.email || !formData.message) {
+        alert(`Fill please all fields`);
+        return;
+    }
+    
+    console.log(formData);
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+    formData.email = "";
+    formData.message = "";
+}
+
+function populateForm() {
+    const saveData = localStorage.getItem(STORAGE_KEY);
+
+    if(saveData) {
+        const saveFormData = JSON.parse(saveData);
+        formData.email = saveFormData.email;
+        formData.message = saveFormData.message;
+
+        email.value = formData.email;
+        textArea.value = formData.message;
+    }
+}
